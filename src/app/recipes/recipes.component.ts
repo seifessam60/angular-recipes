84e4e572e1f87b572f605a2fdb70f94ccbdc412d
seif essam <seifessam60@gmail.com>
2024-12-03 23:21:09 +0200

    initial

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  standalone: false,
+  
+  templateUrl: './recipes.component.html',
+  styleUrl: './recipes.component.css'
+})
+export class RecipesComponent {
+
+}
