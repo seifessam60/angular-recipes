3e517ef2c2e6f7904f4a4240d65fb2b511b78d7d
seif essam <seifessam60@gmail.com>
2024-12-04 12:02:12 +0200

    section1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: false,
-  
+
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test Recipe',
+      'https://wallpaperaccess.com/full/2917213.jpg'
+    ),
+  ];
 }
